6e52fef8c6648ecf4df20c3123d49826288a3d04
Dave Hubertus <davehubertus@gmail.com>
2017-07-16 13:17:58 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-const path = require('path');
-
-app.use(express.static('index.html'));
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+
+  socket.emit('add/remove to online', socket.id)
+  socket.broadcast.emit('status', `${socket.id} has connected`);
+  socket.on('disconnect', function(msg){
+    console.log('user disconnected');
+    io.emit('add/remove to online', socket.id)
+    socket.broadcast.emit('status', `${socket.id} has disconnected`);
+  });
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+  socket.on('user typing', function() {
+    socket.broadcast.emit('typing', 'Someone is typing!')
+  })
+  socket.on('say to someone', (id, msg) => {
+    // send a private message to the socket with the given id
+    socket.to(id[0]).emit('private message', 'this is a private message');
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+io.emit('some event', { for: 'everyone' });
